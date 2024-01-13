fba7dc830b2447c858dd8c00e25f74b8050335ed
Antonio Kim <antonio_kim@outlook.com>
2024-01-12 22:04:32 -0800

    adding MODE and ODR registers for setup

--- Src/main.c
+++ Src/main.c
@@ -11,6 +11,15 @@
 // RCC is also needed (RCC - Reset and Control Clock)
 // 		- AHB1 Bus 0x4002_1000 - 0x400213FF
 // RCC requires to enable clocks for the buses
+// NB: L476 has different registers for RCC
+//
+// GPIO has at least two registers: direction and data
+// direction is used to set the pin as input or output
+// data register is used to store the data
+//
+// Must enable both the mode register and the output data
+// registers, which are called MODER and ODR registers,
+// respectively
 
 #define PERIPH_BASE 		(0x40000000UL)
 #define AHB1PERIPH_OFFSET	(0x00020000UL)
@@ -24,3 +33,16 @@
 #define GPIOB_BASE			(AHB2PERIPH_BASE + GPIOB_OFFSET)
 #define RCC_OFFSET			(0x00021000UL)
 #define RCC_BASE			(AHB1PERIPH_BASE + RCC_OFFSET)
+
+#define AHB2EN_R_OFFSET		(0x4CUL)
+#define RCC_AHB2EN_R		(*(volatile unsigned int *)(RCC_BASE + AHB1EN_R_OFFSET))
+
+#define GPIOAEN				(1U<<0)
+
+#define MODE_R_OFFSET		(0x00UL)
+#define GPIOA_MODE_R		(*(volatile unsigned int *)(GPIOA_BASE + MODE_R_OFFSET))
+
+#define OD_R_OFFSET			(0x14UL)
+#define GPIOA_OD_R			(*(volatile unsigned int *)(GPIOA_BASE + OD_R_OFFSET))
+#define PIN5				(1U<<5)
+#define LED_PIN				PIN5
