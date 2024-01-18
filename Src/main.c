39a41196dc0eb879c08c257a6ab7230c0a426325
Antonio-Kim <antoniok@sfu.ca>
2024-01-17 22:26:44 -0800

    refactoring code
    Removing some of the unused definitions, and cleaning up the code

--- Src/main.c
+++ Src/main.c
@@ -2,36 +2,29 @@
 
 //#include "main.h"
 
-#define PERIPH_BASE                 (0x40000000UL)
-#define AHB1PERIPH_BASE             (PERIPH_BASE + 0x00020000UL)
-#define AHB2PERIPH_BASE             (PERIPH_BASE + 0x08000000UL)
-#define GPIOA_BASE                  (AHB2PERIPH_BASE + 0x0000UL)
-#define RCC_BASE 			        (AHB1PERIPH_BASE + 0x1000UL)
+#define PERIPHBASE			(0x40000000UL)
+#define AHB1_BASE			(0x40020000UL)
+#define AHB2_BASE			(0x40010000UL)
+#define GPIOA_BASE			(0x48000000UL)
+#define RCC_BASE			(0x40021000UL)
 
-#define RCC_AHB2ENR_R			    (*(volatile unsigned int *)(RCC_BASE + 0x4CUL))
-#define RCC_AHB2ENR_GPIOAEN_Pos     (0U)
-#define RCC_AHB2ENR_GPIOAEN_Msk     (0x1UL << RCC_AHB2ENR_GPIOAEN_Pos) 
-#define RCC_AHB2ENR_GPIOAEN         RCC_AHB2ENR_GPIOAEN_Msk
+#define RCC_AHB2ENR			(*(volatile unsigned int *) (RCC_BASE + 0x4C))
+#define GPIOA_MODER			(*(volatile unsigned int *) (GPIOA_BASE + 0x00))
+#define GPIOA_ODR			(*(volatile unsigned int *) (GPIOA_BASE + 0x14))
 
-#define MODE_R_OFFSET		        (0x00UL)
-#define GPIOA_MODE_R		        (*(volatile unsigned int *)(GPIOA_BASE + MODE_R_OFFSET)) 
-#define GPIO_ODR_R			        (*(volatile unsigned int *)(GPIOA_BASE + 0x14UL))
 
-#define GPIO_ODR_OD5_Pos            (5U)
-#define GPIO_ODR_OD5_Msk            (0x1UL << GPIO_ODR_OD5_Pos) 
-#define GPIO_ODR_PIN5               GPIO_ODR_OD5_Msk
-
-#define RCC							((RCC_TypeDef *) RCC_BASE)
-#define GPIOA						((GPIO_TypeDef *) GPIOA_BASE)
+#define GPIOAEN				(1U<<0)
+#define PIN_5				(1U<<5)
+#define LED_PIN				PIN_5
 
 int main(void) {
-	 RCC_AHB2ENR_R |= RCC_AHB2ENR_GPIOAEN;
-	 GPIOA_MODE_R |= (1U<<10);
-	 GPIOA_MODE_R &= ~(1U<<11);
-	 while (1) {
-	 	GPIO_ODR_R ^= GPIO_ODR_PIN5;
-	 	for (size_t i = 0; i < 1000000;i++) {}
-	 }
+    	RCC_AHB2ENR |= GPIOAEN;
+	GPIOA_MODER |= (1U<<10);
+	GPIOA_MODER &= ~(1U<<11);
+	while (1) {
+		GPIOA_ODR ^= LED_PIN;
+		for (size_t i = 0; i < 1000000;i++) {}
+	}
 
 //	 Use the following below if you're using main.h
 //
